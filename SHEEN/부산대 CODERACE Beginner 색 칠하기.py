a828561cfb05c9eb763a98132db44d0f93a9214d
SHEEN <normengdie@pusan.ac.kr>
2024-05-01 15:11:01 +0900

    Create 부산대 CODERACE Beginner 색 칠하기.py

---
+++ SHEEN/부산대 CODERACE Beginner 색 칠하기.py
@@ -0,0 +1,37 @@
+a, b =map(int,input().split())
+maps = []
+count = 0
+q =0
+w = 0
+ccount = 0
+for i in range(a):
+    smallMaps = list(map(int,input().split()))
+    maps.append(smallMaps)
+
+
+
+def solution(maps,w,ccount,q):
+    for zz in range(len(maps)):
+        q =0
+        w = 0
+        while q < b:
+            if maps[zz][q] ==0:
+                w = maps[zz][q]
+                q+=1    
+                continue
+            else:
+
+                if w ==maps[zz][q]:
+                    w = maps[zz][q]
+                    q+=1
+                    continue
+                else:
+                
+                    ccount +=1
+                    w = maps[zz][q]
+                    q+=1
+                    continue
+                    
+    return ccount    
+
+print(solution(maps,w,ccount,q))
