3c016dee634f10ced06a068600cdd549585b7946
SHEEN <normengdie@pusan.ac.kr>
2024-05-03 20:25:07 +0900

    Update 11478 서로 다른 부분 문자열의 개수.py

--- SHEEN/11478 서로 다른 부분 문자열의 개수.py
+++ SHEEN/11478 서로 다른 부분 문자열의 개수.py
@@ -1,11 +1,14 @@
-from itertools import combinations
+strings = input()
 
-
-A = input()
-array = []
-A = ''.join(sorted(A))
-print(A)
-A = list(combinations(A, 2))
-for k in A:
-    tup = ''.join(k)
-    print(tup)
+nomordic = {}
+plus = 0
+for i in range(1,len(strings)+1):
+  p = 0
+  while p+i <= len(strings):
+    if strings[p:p+i] in nomordic:
+      nomordic[strings[p:p+i]] += 1
+    else:
+       nomordic[strings[p:p+i]] = 1
+    p+=1   
+print(len(nomordic))
+    
