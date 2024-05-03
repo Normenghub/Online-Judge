72906dc6303cca53c43761cc12a61c9f0adf1494
SHEEN <161808007+Normenghub@users.noreply.github.com>
2024-05-03 16:09:14 +0900

    Create 9012 괄호.py

---
+++ SHEEN/9012 괄호.py
@@ -0,0 +1,23 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+lists = deque()
+
+num= int(input())
+
+
+def solution(num,lists):
+        strings = list(input().split())
+         for k in range(len(strings)):
+            try:
+             if k == '(':
+                 lists.append(k)
+             else:
+                 lists.pop()
+            except:
+                return "NO"
+         return "Yes"        
+        
+for i in range(num):
+    print(solution(num,lists))            
