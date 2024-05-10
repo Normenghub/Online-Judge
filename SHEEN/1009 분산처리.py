e2ef83c41ea20fc85307984ba71ccb16e616556c
SHEEN <normengdie@pusan.ac.kr>
2024-05-11 00:31:57 +0900

    .

--- SHEEN/1009 분산처리.py
+++ SHEEN/1009 분산처리.py
@@ -1,23 +1,19 @@
-n = int(input())
-
-for i in range(n):
- c = []
- a ,b = map(int,input().split())
- if b > 1 and a !=1:
-  while b<=1:
-    c.append(b//2)
-    b -= (b//2)
-  c.append(b)
-  result=0
-  for i in c:
-   if i * a >= 10:
-     result += (a**i) % 10
-   else:
-     result += (a*i)
-  print(result)    
- elif a ==1 and b !=1:
-   print(1)
- else:
-   print(0)  
-
-  
+num=int(input()) 
+for i in range(num): 
+    a,b=map(int,input().split()) 
+    a=a%10 
+    if a==1 or a==5 or a==6: 
+    	print(a) 
+    
+    elif a==2 or a==3 or a==7 or a==8: 
+        if b%4==0: 
+         print(a**4%10) 
+        else: 
+         print((a**(b%4))%10) 
+    elif a==4 or a==9: 
+        if b%2==0: 
+         print(a**2%10) 
+        else: 
+         print((a**(b%2))%10) 
+    else: 
+     print('10')
