36a1f2ae6bbdbcd98548b21c6648dba2ba06a0f1
SHEEN <normengdie@pusan.ac.kr>
2024-07-07 16:11:59 +0900

    Update workspace.py

--- workspace.py
+++ workspace.py
@@ -1,1 +1,10 @@
-from itertools import permutations
+ff = []
+ff.append([False] * 6)
+ff.append([False] * 6)
+ff.append([False] * 6)
+ff.append([False] * 6)
+ff.append([False] * 6)
+ff.append([False] * 6)
+print(ff)
+ff[0][0] = 1
+print(ff)
