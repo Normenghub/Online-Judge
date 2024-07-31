4ee5a2fd6b5d03a7ad66cd741df84fbc9a9c871b
SHEEN <normengdie@pusan.ac.kr>
2024-07-31 17:01:39 +0900

    Create 2225 합분해.py

---
+++ 2225 합분해.py
@@ -0,0 +1,15 @@
+import sys
+input =sys.stdin.readline
+
+n,k = map(int,input().split())
+
+dp = [[0] * (n+1) for i in range(k+1)]
+
+for i in range(1,k+1):
+    dp[i][1] = i
+    dp[i][0] = 1
+for i in range(1,k+1):
+    for z in range(2,n+1):
+        dp[i][z] = dp[i][z-1] + dp[i-1][z]
+print(dp)
+print(dp[k][n]%1000000000)
